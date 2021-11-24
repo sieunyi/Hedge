a8aa62d9dc046399bee1b289821fd7b70081ff2d
unknown <sleee@tamu.edu>
2021-11-23 21:54:23 -0600

    Removal of unnecessary files & Roxygen2

--- R/hedge.R
+++ R/hedge.R
@@ -1,6 +1,19 @@
 
+#' minimum variance hedge ratio function
+#'
+#' @param x Inputs x is a raw data consists of spot price in the first column and futures prices from the second column.
+#' @param WinLen Length of window
+#'
+#' @return return(HR)
+#' @export
+#'
+#' @examples
+#'  x = matrix(rnorm(100), 50, 2)
+#'  Winlen = 15
+#'  mvhr(x, Winlen)
+#'
 mvhr <- function(x, WinLen){
-  # Inputs x is a raw data consists of spot price in the first column and futures prices from the second column.
+
   # Make the dataset balanced & remove NaN observations
   # drop the observation with NaN values
   x <- na.omit(x)
@@ -38,34 +51,37 @@ mvhr <- function(x, WinLen){
   histFut = ld_fp[iWin:iWin+WinLen-1,]
   HR[iWin,] =  sd(histSpot)*cor(histSpot,histFut)/sd(histFut)
   }
-
-
+return(HR)
+}
 # Performance of the hedge ratio
 # profit (hedgers hold the contract for 1 day and resell it.)
 HE = matrix(0, nrow = nWin, ncol= nFut)
 
 for (iWin in 1:nWin){
-OoSSpot = ld_btcs[iWin+WinLen:iWin+WinLen+OoSLen-1,]
-OoSFut = ld_fp[iWin+WinLen:iWin+WinLen+OoSLen-1,]
-hr_mod = HR[iWin,]*fp(iWin+WinLen,)/btcs(iWin+WinLen)
-Prof_nh = OoSSpot
-Prof_h = repmat(OoSSpot,1,nFut) - repmat(hr_mod,OoSLen,1).*OoSFut
-var_h[iWin,] = var(Prof_h)
-var_nh[iWin,] = var(Prof_nh)
-HE[iWin,] = (var(Prof_h)-var(Prof_nh))/var(Prof_nh)
-}
- return(list(HR = HR, HE = HE))
+  OoSSpot = ld_btcs[iWin+WinLen:iWin+WinLen+OoSLen-1,]
+  OoSFut = ld_fp[iWin+WinLen:iWin+WinLen+OoSLen-1,]
+  hr_mod = HR[iWin,]*fp(iWin+WinLen,)/btcs(iWin+WinLen)
+  Prof_nh = OoSSpot
+  Prof_h = repmat(OoSSpot,1,nFut) - repmat(hr_mod,OoSLen,1)*OoSFut
+  var_h[iWin,] = var(Prof_h)
+  var_nh[iWin,] = var(Prof_nh)
+  HE[iWin,] = (var(Prof_h)-var(Prof_nh))/var(Prof_nh)
 }
 
 
-# Semivariance hedge ratio
-
-# initialize the matrix
-# Semivariance hedge ratio
+#' Semivariance hedge ratio
+#'
+#' @param v price matrix
+#' @param w weight matrix
+#'
+#' @return semivariance vector SV
+#' @export
+#'
+#' @examples
 semivar <- function(v,w){
-meanV = w'*v
-SV = w'*(max(repmat(meanV,size(v,1),1)-v, 0)).^2
-return(SV)
+  meanV = tcrossprod(w,v)
+SV = tcrossprod(w, (max(repmat(meanV,size(v,1),1)-v, 0))^2)
+  return(SV)
 }
 
 # initialize the matrix
@@ -74,23 +90,23 @@ HR_sv =  zeros(nWin,nFut)
 HE_sv = zeros(nWin,nFut)
 
 for (iWin in 1:nWin){
-histSpot = ld_btcs(iWin:iWin+WinLen-1,:)
-wHist =  ones(WinLen,1)/WinLen   # weight
-wOoS  = ones(OoSLen,1)/OoSLen
-OoSSpot = ld_btcs(iWin+WinLen:iWin+WinLen+OoSLen-1,:)
-OoSFut = ld_fp(iWin+WinLen:iWin+WinLen+OoSLen-1,:)
+  histSpot = ld_btcs(iWin:iWin+WinLen-1,)
+  wHist =  ones(WinLen,1)/WinLen   # weight
+  wOoS  = ones(OoSLen,1)/OoSLen
+  OoSSpot = ld_btcs(iWin+WinLen:iWin+WinLen+OoSLen-1,)
+  OoSFut = ld_fp(iWin+WinLen:iWin+WinLen+OoSLen-1,)
 
-# semivariance with no hedging
-sv_nh = semivar(OoSSpot, wOoS)
+  # semivariance with no hedging
+  sv_nh = semivar(OoSSpot, wOoS)
 
-for (iFut in 1: nFut){
-SVObj = @(h)SemiVar(OoSSpot - h*(fp(iWin+WinLen,iFut)/btcs(iWin+WinLen))*OoSFut(:,iFut),wHist)
-HR_sv(iWin,iFut) = fminsearch(SVObj,HR0)
-}
+#  for (iFut in 1: nFut){
+ #   SVObj = (h)SemiVar(OoSSpot - h*(fp(iWin+WinLen,iFut)/btcs(iWin+WinLen))*OoSFut(:,iFut),wHist)
+ #   HR_sv[iWin,iFut] = fminsearch(SVObj,HR0)
+#  }
 
-Prof_h_sv = repmat(OoSSpot,1,nFut) - repmat(HR_sv(iWin,:).*(fp(iWin+WinLen,:)/btcs(iWin+WinLen)),OoSLen,1).*OoSFut
-sv_h = SemiVar(Prof_h_sv, wOoS)
-HE_sv(iWin,:) = (sv_h-sv_nh)/(sv_nh)
-return()
+  Prof_h_sv = repmat(OoSSpot,1,nFut) - repmat(HR_sv(iWin,)*(fp(iWin+WinLen,)/btcs(iWin+WinLen)),OoSLen,1)*OoSFut
+  sv_h = SemiVar(Prof_h_sv, wOoS)
+  HE_sv[iWin,] = (sv_h-sv_nh)/(sv_nh)
+  return()
 }
 
