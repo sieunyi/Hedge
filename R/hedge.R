62faa9c9180395cc85357ec8790864dd680110d4
unknown <sleee@tamu.edu>
2021-12-11 23:14:08 -0600

    functions run without without errors & data in rda format

--- R/hedge.R
+++ R/hedge.R
@@ -1,14 +1,16 @@
 #' minimum variance hedge ratio function
 #'
-#' @param x Inputs x is a raw data consists of spot price in the first column and futures prices from the second column.
+#' @param x (n x 2) matrix of input that consists of spot price in the first column and futures prices in the second column.
 #' @param WinLen Length of window
 #'
-#' @return return(HR <- matrix size 언급해주기)
+#' @return (list(HR = HR, HE = HE))
+#' \item{HR}{Length(nWin x 1) optimal hedge ratio matrix}
+#' \item{HE}{Length(nWin x 1) hedging effectiveness matrix}
 #' @export
 #' @references
 #'
 #' @examples
-#' x <- matrix(rnorm(100), 50, 2)
+#' x <- matrix(runif(500, min=0, max=10), 500, 2)
 #' Winlen <- 15
 #' mvhr(x, Winlen)
 mvhr <- function(x, WinLen) {
@@ -29,11 +31,11 @@ mvhr <- function(x, WinLen) {
   # futures price
   fp <- x[, 2:p]
   # take the log difference to remove non-stationarity
-  lsp <- log(sp)
-  ld_sp <- lsp[2:n, ] - lsp[1:(n - 1), ]
-  lfp <- log(fp)
-  ld_fp <- lfp[2:n, ] - lfp[1:(n - 1), ]
+  ld_sp <- diff(log(sp))
+  ld_fp <- diff(log(fp))
 
+  ld_sp <- as.matrix(ld_sp)
+  sp <- as.matrix(sp)
 
   # Minimum variance Hedge ratio
   # WinLen = 15       # window length
@@ -42,9 +44,14 @@ mvhr <- function(x, WinLen) {
   # Number of windows
   nObs <- nrow(ld_sp)
   # Number of futures contracts
-  nFut <- ncol(ld_fp)
+  nFut <- p-1
   # Total number of windows
   nWin <- nObs - WinLen - OoSLen + 1
+
+  if (nFut==1){
+    ld_fp = as.matrix(ld_fp)
+    fp = as.matrix(fp)
+  }
 
   # construct an empty storage for HR, history data
   HR <- matrix(0, nrow = nWin, ncol = nFut)
@@ -52,11 +59,12 @@ mvhr <- function(x, WinLen) {
   histFut <- matrix(0, nrow = WinLen, ncol = 1)
   HE <- matrix(0, nrow = nWin, ncol = nFut)
   var_h <- matrix(0, nrow = nWin, ncol = nFut)
-    var_nh <- matrix(0, nrow = nWin, ncol = nFut)
+  var_nh <- matrix(0, nrow = nWin, ncol = nFut)
 
 
   for (iWin in 1:nWin) {
     # historical spot price
+
     histSpot <- ld_sp[iWin:(iWin + WinLen - 1), ]
     histFut <- ld_fp[iWin:(iWin + WinLen - 1), ]
     HR[iWin, ] <- (sd(histSpot) * cor(histSpot, histFut)) / sd(histFut)
@@ -68,6 +76,7 @@ mvhr <- function(x, WinLen) {
     OoSFut <- ld_fp[(iWin + WinLen):(iWin + WinLen + OoSLen - 1), ]
     # getting a modified hedge ratio
     # hr_mod is a scalar
+
     hr_mod <- HR[iWin, ] * fp[(iWin + WinLen), ] / sp[(iWin + WinLen), ]
     # prof_nh is length(WinLen) column vector when h = 0, which is equal to spot price.
     Prof_nh <- OoSSpot
@@ -104,7 +113,7 @@ semivar <- function(v, w) {
 
 
 #' Semi variance hedge ratio and its effectiveness
-#'
+#' @inheritParams mvhr
 #' @param x Inputs x is a raw data consists of spot price in the first column and futures prices from the second column.
 #' @param WinLen Length of window
 #'
@@ -113,41 +122,76 @@ semivar <- function(v, w) {
 #'
 #' @examples
 svhr <- function(x, WinLen) {
-  # initialize the matrix
-  #  HR0 <- 1
-  HR_sv <- matrix(0, nWin, nFut)
-  HE_sv <- matrix(0, nWin, nFut)
+
+
+  # Make the dataset balanced & remove NaN observations
+  # drop the observation with NaN values
+  x <- na.omit(x)
+  # Check the compatibility of WinLen
+  if (WinLen <= 0) {
+    stop("Window Length should be positive")
+  }
+
+  # variable setting
+  p <- ncol(x)
+  n <- nrow(x)
+  # spot price
+  sp <- x[, 1]
+  # futures price
+  fp <- x[, 2:p]
+  # take the log difference to remove non-stationarity
+  ld_sp <- diff(log(sp))
+  ld_fp <- diff(log(fp))
+
+  ld_sp <- as.matrix(ld_sp)
+  sp <- as.matrix(sp)
+
+  # Minimum variance Hedge ratio
+  # WinLen = 15       # window length
   # out of sample
   OoSLen <- WinLen
   # Number of windows
   nObs <- nrow(ld_sp)
   # Number of futures contracts
-  nFut <- ncol(ld_fp)
+  nFut <- p-1
   # Total number of windows
   nWin <- nObs - WinLen - OoSLen + 1
 
+  if (nFut==1){
+    ld_fp = as.matrix(ld_fp)
+    fp = as.matrix(fp)
+  }
+
+  # initialize the matrix
+  #  HR0 <- 1
+  HR_sv <- matrix(0, nWin, nFut)
+  HE_sv <- matrix(0, nWin, nFut)
+
+  # construct an empty storage for HR, history data
+  histSpot <- matrix(0, nrow = WinLen, ncol = 1)
+  OoSSpot <- matrix(0, nrow = WinLen, ncol = 1)
+  OoSFut <- matrix(0, nrow = WinLen, ncol = 1)
+  wHist <- (matrix(1, WinLen, 1)) / WinLen
+  num <- matrix(1, nrow = OoSLen, ncol = 1)
+  wOoS <- num / OoSLen
 
   for (iWin in 1:nWin) {
-    histSpot <- ld_sp[iWin:iWin + WinLen - 1, ]
+    histSpot <- as.matrix(ld_sp[iWin:(iWin + WinLen - 1), ])
     # weight for the historical price
-    wHist <- matrix(1, WinLen, 1) / WinLen
-    num <- matrix(1, nrow = OoSLen, ncol = 1)
-    wOoS <- num / OoSLen
-    OoSSpot <- ld_sp[iWin + WinLen:iWin + WinLen + OoSLen - 1, ]
-    OoSFut <- ld_fp[iWin + WinLen:iWin + WinLen + OoSLen - 1, ]
-
+    OoSSpot <- as.matrix(ld_sp[(iWin + WinLen):(iWin + WinLen + OoSLen - 1), ])
+    OoSFut <- as.matrix(ld_fp[(iWin + WinLen):(iWin + WinLen + OoSLen - 1), ])
     for (iFut in 1:nFut) {
-      SVObj <- function(h) semivar(matrix(as.numeric(OoSSpot - h * (fp[iWin + WinLen, iFut] / sp[iWin + WinLen, ]) * as.matrix(OoSFut, nrow = WinLen)[, iFut]), nrow=WinLen, 1), wHist)
+      SVObj <- function(h) semivar(matrix(as.numeric(OoSSpot - h * (fp[(iWin + WinLen), iFut] / sp[(iWin + WinLen), ]) * as.matrix(OoSFut, nrow = WinLen)[, iFut]), nrow=WinLen, 1), wHist)
       result <- optimize(SVObj, interval = c(-10, 10), maximum = FALSE)
-      HR_sv <- result$minimum
+      HR_sv[iWin, iFut] <- result$minimum
     }
-
     # semivariance with no hedging
     sv_nh <- semivar(OoSSpot, wOoS)
-
-    Prof_h_sv <- matrix(OoSSpot, length(OoSSpot), nFut) - as.numeric(matrix(HR_sv[iWin, ] * (fp[iWin + WinLen, ] / sp[iWin + WinLen, ]), length(OoSSpot), nFut)) * as.matrix(OoSFut)
+    Prof_h_sv <- matrix(OoSSpot, length(OoSSpot), nFut) - as.numeric(matrix(HR_sv[iWin, ] * (fp[(iWin + WinLen), ] / sp[(iWin + WinLen), ]), length(OoSSpot), nFut)) * as.matrix(OoSFut)
     sv_h <- semivar(Prof_h_sv, wOoS)
     HE_sv[iWin, ] <- (sv_h - sv_nh) / (sv_nh)
   }
-  return(HR_sv = HR_sv, HE_sv = HE_sv)
+
+  return(list(HR_sv = HR_sv, HE_sv = HE_sv))
+
 }
